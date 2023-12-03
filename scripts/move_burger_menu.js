46aaa3bb773cbbc00c824101a431cfeb13148aa8
Denis Bikmaev <dbikmaev2003@gmail.com>
2023-12-03 17:37:29 +0300

    Added terms page

--- scripts/move_burger_menu.js
+++ scripts/move_burger_menu.js
@@ -5,8 +5,33 @@ const menu = document.querySelector("#menu");
 
 navButton.addEventListener("change", () => {
     if (navButton.checked) {
-        menu.style.transform = "none";
+        menu.style.transform = "translate(0, 0)";
     } else {
         menu.style.transform = "translate(100%, 0)";
     }
 });
+
+// Функция сворачивания навигационного окна при адаптивном переходе
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 800) {
+        menu.style = "none";
+        navButton.checked = false;
+    }
+});
+
+// Отслеживание нажатий кнопок навигации для сворачивания окна навигации
+let buttons = document.querySelectorAll(".nav-btn");
+for (let index = 0; index < buttons.length; index++) {
+    let button = buttons[index];
+    button.addEventListener('click', function (event) {
+        console.log('click');
+        if (window.innerWidth <= 800) {
+            setTimeout(() => {
+                menu.style.transform = "translate(100%, 0)";
+                navButton.checked = false;
+                event.preventDefault();
+            }, 10);
+            
+        }
+    });
+}
